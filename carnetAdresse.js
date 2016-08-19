87f2b3e336ab2d17df2b8e417f93191ce38e0512
Isabelle Lechat <Idariel@users.noreply.github.com>
2016-08-20 00:39:09 +0200

    Update carnetAdresse.js

--- carnetAdresse.js
+++ carnetAdresse.js
@@ -1,3 +1,23 @@
+// var titreListe = document.createElement("h1");
+//   titreListe.innerHTML = "Carnet d'Adresses";
+// document.body.appendChild(titreListe);
+
+var titre = document.write("<h1>Carnet d'Adresses<h1>")
+// Div liste existante
+// 1 - Affichage de la liste existante
+// Div Ajouter un contact
+
+// Création de la div dans laquelle s'affiche la liste de contacts
+var divListe = document.createElement("div");
+  divListe.id = "listeContact";
+  divListe.className = "txt";
+  divListe.backgroundColor = "#333333";
+document.body.appendChild(divListe);
+var ul = document.createElement("ul");
+  ul.className = "txt";
+divListe.appendChild(ul);
+
+// Creation de la liste de contact originale (Bob et Mary)
 var contactList = [];
 
 var newContact = function (firstName,lastName,phoneNumber,email) {
@@ -12,8 +32,13 @@ var mary = new newContact ("Mary","Johnson","(650) 888-8888","mary.johnson@examp
 
 contactList.push(bob, mary);
 
+// Création du nom affiché
 function printPerson(person) {
-    console.log(person.firstName + " " + person.lastName);
+  var li = document.createElement("li");
+    li.className = "txt";
+  ul.appendChild(li);
+    li.innerHTML = person.firstName + " " + person.lastName;
+  console.log(person.firstName + " " + person.lastName);
 };
 
 var list = function() {
@@ -22,27 +47,28 @@ var list = function() {
 	};
 };
 
-// list();
+list();
+
 
 // /*Create a search function
 // then call it passing "Jones"*/
-var search = function(lastName) {
-	for (var i = 0; i < contactList.length; i++) {
-		if (lastName === contactList[i].lastName) {
-			printPerson(contactList[i]);
-			return;
-		};
-	};
-};
-search("Jones");
-
-var add = function(firstName,lastName,email,phoneNumber) {
-  var newContact = new Object();
-  newContact.firstName = firstName;
-  newContact.lastName = lastName;
-  newContact.email = email;
-  newContact.phoneNumber = phoneNumber;
-  contactList.push(newContact);
-  list();
-};
-add("Isabelle","Leleu", "isolde@toto.com","0123456")
+// var search = function(lastName) {
+// 	for (var i = 0; i < contactList.length; i++) {
+// 		if (lastName === contactList[i].lastName) {
+// 			printPerson(contactList[i]);
+// 			return;
+// 		};
+// 	};
+// };
+// search("Jones");
+//
+// var add = function(firstName,lastName,email,phoneNumber) {
+//   var newContact = new Object();
+//   newContact.firstName = firstName;
+//   newContact.lastName = lastName;
+//   newContact.email = email;
+//   newContact.phoneNumber = phoneNumber;
+//   contactList.push(newContact);
+//   list();
+// };
+// add("Isabelle","Leleu", "isolde@toto.com","0123456")
